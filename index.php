dcec6a519e1bc5eb47ab4ffcf087eb37957a8c35
PL-29 <pl.ollivier29@gmail.com>
2015-10-27 19:14:38 +0100

    vue déplacée

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
-include("vues/v_connexion.php");
+	
 	session_start();
 	
 	
 	if(!isset($_REQUEST["uc"])){
 		$_REQUEST["uc"] = 'connexion';
-	}	 
+	}
+	include("vues/v_connexion.php");
 	$uc = $_REQUEST["uc"];
 	switch($uc){
 		case 'connexion':{
