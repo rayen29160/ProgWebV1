49b9f7533009b3ca9cab04634db31406267ad8ce
PL-29 <pl.ollivier29@gmail.com>
2015-10-28 16:01:24 +0100

    Modification controleur et fonction

--- controleurs/c_connexion.php
+++ controleurs/c_connexion.php
@@ -15,14 +15,16 @@ switch ($action) {
 		{
 			include ('include/fonctions.php');
 			$id = $_REQUEST ['id'];
-			$mdp = $_REQUEST ['mdp'];
-			var_dump($id);
+			$mdp = $_REQUEST ['mdp'];			
 			
-			if(!testConnexion( $id, $mdp ))
+			//Teste si l'identifiant et le mot de passe sont bons
+			if(testConnexion($id,$mdp))
 			{
+				//Si oui, affiche la page d'accueil et le sommaire
 				include('vues/v_accueil.php');
 				include('vues/v_sommaire.php');
 			} else {
+				//Sinon on reste sur la page de connexion
 				include('vues/v_connexion.php');				
 			}
 			break;
--- include/fonctions.php
+++ include/fonctions.php
@@ -1,10 +1,10 @@
 <?php
-	function testConnexion($id, $mdp){
-		
-		if($id=="abc" && mdp=="abc") {
-			echo "win".$id." ".$mdp;
+	//Renvoie true si l'identifiant et le mot de passe sont bons
+	function testConnexion($id, $mdp){		
+		if($id=="abc" && $mdp=="abc") {
+			return true;
 		} else {
-			echo "fail".$id." ".$mdp;
+			return false;
 		}
 	}
 	
