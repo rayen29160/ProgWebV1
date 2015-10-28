2f54e3a3c722a4c87623b3ee9e737e936652d5fd
PL-29 <pl.ollivier29@gmail.com>
2015-10-28 11:52:30 +0100

    Essai ajout image dans liste déroulante

--- scripts/verifChamps.js
+++ scripts/verifChamps.js
@@ -1,4 +1,3 @@
-
 function estVide(champId) {
 	var value = document.getElementById(champId).value;
 	value = value.trim();
@@ -11,7 +10,7 @@ function estVide(champId) {
 }
 
 function verificationRemplissage(id1, id2, id3, id4) {
-	//var value1 = document.getElementById(id1).value;
+	// var value1 = document.getElementById(id1).value;
 	// var value2 = document.getElementById(id2).value;
 	// var value3 = document.getElementById(id3).value;
 	// var value4 = document.getElementById(id4).value;
--- vues/v_inscription.php
+++ vues/v_inscription.php
@@ -11,24 +11,97 @@
 		Combien de cigarettes fumez-vous par jour ?*  <input type="text" id="nbCigarettes" name="nbCigarettes"/><br><br>		
 		
 		Quelle marque de cigarettes achetez-vous ?* 
-		<select id="test" name="listeMarques">
-			<option id="0" selected> --- Votre marque de cigarette --- </option>
-			<option id="1">Marlboro</option>
-			<option id="2">Lucky Strike</option>
-			<option id="3">Gitanes</option>
-			<option id="4">Philip Maurice</option>
-			<option id="5">Camel</option>
-			<option id="6">Royale</option>
-			<option id="7">Gauloises</option>
-			<option id="8">Bastos</option>
-			<option id="9">News</option>
-			<option id="10">...</option>
-		</select>
-		<br><br>
+		<!--  <select id="test" name="listeMarques" onChange="changeImage()">
+			<option value="0" selected>--- Votre marque de cigarette ---</option>
+			<option value="1"><img src="images/Marloboro.PNG"/>Marlboro</option>
+			<option value="2">Lucky Strike</option>
+			<option value="3">Gitanes</option>
+			<option value="4">Philip Maurice</option>
+			<option value="5"><img src="images/Camel.PNG"/></option>
+			<option value="6">Royale</option>
+			<option value="7">Gauloises</option>
+			<option value="8">Bastos</option>
+			<option value="9">News</option>
+			<option value="10">...</option>
+		</select>-->
+		
+		
 		
 		<font color="red">Les champs * sont des champs obligatoires</font>
 		
-		<script type="text/javascript" src="scripts/scripts.js"></script>
+		<!-- <script type="text/javascript" src="scripts/verifChamps.js"></script> -->
+		
+		<script language="javascript">
+			//Enlève les espaces et renvoie true si le champ est vide
+			function estVide(champId) {
+				var value = document.getElementById(champId).value;
+				value = value.trim();
+
+				if (value === "") {
+					return true;
+				} else {
+					return false;
+				}
+			}
+
+
+			//Verifie que les champs ne sont pas vides. Si ils le sont leur background devient rouge
+			function verificationRemplissage(id1, id2, id3, id4) {
+
+				var selectedValue = document.getElementById(id4).value;
+				
+				if(estVide(id1)) {
+					document.getElementById(id1).style.backgroundColor='red';					
+				}
+
+				if(estVide(id2)) {
+					document.getElementById(id2).style.backgroundColor='red';
+				}
+
+				if(estVide(id3)) {
+					document.getElementById(id3).style.backgroundColor='red';
+				}
+
+				
+				//if(selectedValue==="0") {
+					//document.getElementById(id4).style.backgroundColor='red;
+				//}
+			}
+
+			function changeImage() {
+					var select = document.getElementById("test");
+					style = select.style;
+
+					
+					switch(select.selectedIndex) {
+					case 0:
+						style.background = "url('images/Marlboro.PNG') no-repeat";
+						alert("aaaa");
+						break;
+					case 1:
+						style.background = "url('images/LuckyStrike.PNG') no-repeat";
+						break;
+					case 2:
+						style.background = "url('images/Camel.PNG') no-repeat";
+						break;
+					}
+			}
+
+			
+		</script>
+		
+		
+		<select id="test" name="listeMarques" onChange="changeImage();">			
+			<option style="background:url('images/Marlboro.PNG') no-repeat; width:250px; height:100px;"></option>
+			<option style="background:url('images/LuckyStrike.PNG') no-repeat; width:250px; height:100px;"></option>			
+			<option style="background:url('images/Camel.PNG') no-repeat; width:250px; height:100px;"></option>
+			
+		</select>
+		
+		
+		<br><br>
+		
 		<input type="button" onClick="verificationRemplissage('age', 'ageDebut', 'nbCigarettes', 'test');" value="Valider le formulaire"/>
+		<!-- <input type="button" onClick="estVide('age');" value="Valider le formulaire"/> -->
 	</body>
 </html>
