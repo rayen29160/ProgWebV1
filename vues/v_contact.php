24ce92c82e581796aae7e5fe90ad812c4c840aac
PL-29 <pl.ollivier29@gmail.com>
2015-10-28 18:45:31 +0100

    Formulaire de contact

---
+++ vues/v_contact.php
@@ -0,0 +1,11 @@
+<html>
+	<head><h1>Nous contacter</h1></head>
+	<body>
+		<div>
+			Votre nom* : <input type="text" id="nom"/><br><br>
+			Votre adresse email* : <input type="text" id="email"/><br><br>
+			Votre message* : <input type="textarea" id="message"/><br><br>
+			<input type="submit" value="Envoyer"/>		
+		</div>	
+	</body>
+</html>
