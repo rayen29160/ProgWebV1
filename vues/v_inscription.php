a29f44812340b1a39d1597246ad7fff806a5f669
PL-29 <pl.ollivier29@gmail.com>
2015-10-28 13:35:17 +0100

    modif formulaire inscription

--- vues/v_inscription.php
+++ vues/v_inscription.php
@@ -1,37 +1,6 @@
 <html>
-	<head><h1>Remplissez ce formulaire</h1></head>
-	<body>
-		
-		
-		
-		Quel âge avez-vous ?* <?php echo 'Peut être une liste déroulante avec choix date naissance ?'?><input type="text" id="age" name="age"/><br><br>
-		
-		Age auquel vous avez commencé ?*  <input type="text" id="ageDebut" name="ageDebut"/><br><br>
-		
-		Combien de cigarettes fumez-vous par jour ?*  <input type="text" id="nbCigarettes" name="nbCigarettes"/><br><br>		
-		
-		Quelle marque de cigarettes achetez-vous ?* 
-		<!--  <select id="test" name="listeMarques" onChange="changeImage()">
-			<option value="0" selected>--- Votre marque de cigarette ---</option>
-			<option value="1"><img src="images/Marloboro.PNG"/>Marlboro</option>
-			<option value="2">Lucky Strike</option>
-			<option value="3">Gitanes</option>
-			<option value="4">Philip Maurice</option>
-			<option value="5"><img src="images/Camel.PNG"/></option>
-			<option value="6">Royale</option>
-			<option value="7">Gauloises</option>
-			<option value="8">Bastos</option>
-			<option value="9">News</option>
-			<option value="10">...</option>
-		</select>-->
-		
-		
-		
-		<font color="red">Les champs * sont des champs obligatoires</font>
-		
-		<!-- <script type="text/javascript" src="scripts/verifChamps.js"></script> -->
-		
-		<script language="javascript">
+	<head><h1>Remplissez ce formulaire</h1>
+	<script language="javascript">
 			//Enlève les espaces et renvoie true si le champ est vide
 			function estVide(champId) {
 				var value = document.getElementById(champId).value;
@@ -75,28 +44,63 @@
 					
 					switch(select.selectedIndex) {
 					case 0:
-						style.background = "url('images/Marlboro.PNG') no-repeat";
-						alert("aaaa");
 						break;
 					case 1:
-						style.background = "url('images/LuckyStrike.PNG') no-repeat";
+						style.background = "url('../images/Marlboro.PNG') no-repeat;";						
 						break;
 					case 2:
-						style.background = "url('images/Camel.PNG') no-repeat";
+						style.background = "url('../images/LuckyStrike.PNG') no-repeat";
+						break;
+					case 3:
+						style.background = "url('../images/Camel.PNG') no-repeat";
 						break;
 					}
 			}
 
 			
 		</script>
+		</head>
+	<body>
 		
 		
-		<select id="test" name="listeMarques" onChange="changeImage();">			
-			<option style="background:url('images/Marlboro.PNG') no-repeat; width:250px; height:100px;"></option>
-			<option style="background:url('images/LuckyStrike.PNG') no-repeat; width:250px; height:100px;"></option>			
-			<option style="background:url('images/Camel.PNG') no-repeat; width:250px; height:100px;"></option>
-			
+		
+		Quel âge avez-vous ?* <?php echo 'Peut être une liste déroulante avec choix date naissance ?'?><input type="text" id="age" name="age"/><br><br>
+		
+		Age auquel vous avez commencé ?*  <input type="text" id="ageDebut" name="ageDebut"/><br><br>
+		
+		Combien de cigarettes fumez-vous par jour ?*  <input type="text" id="nbCigarettes" name="nbCigarettes"/><br><br>		
+		
+		Quelle marque de cigarettes achetez-vous ?* 
+		<!--  <select id="test" name="listeMarques" onChange="changeImage()">
+			<option value="0" selected>--- Votre marque de cigarette ---</option>
+			<option value="1"><img src="images/Marloboro.PNG"/>Marlboro</option>
+			<option value="2">Lucky Strike</option>
+			<option value="3">Gitanes</option>
+			<option value="4">Philip Maurice</option>
+			<option value="5"><img src="images/Camel.PNG"/></option>
+			<option value="6">Royale</option>
+			<option value="7">Gauloises</option>
+			<option value="8">Bastos</option>
+			<option value="9">News</option>
+			<option value="10">...</option>
+		</select>-->
+		
+		
+		
+		<font color="red">Les champs * sont des champs obligatoires</font>
+		
+		<!-- <script type="text/javascript" src="scripts/verifChamps.js"></script> -->
+		
+		
+		
+		
+		<select id="test" name="listeMarques" onChange="changeImage();" style="width:250px; height:150px;">
+		  	<option selected>--Choisissez votre marque--</option>
+			<option style="background:url('../images/Marlboro.PNG') no-repeat;"></option>
+			<option style="background:url('../images/LuckyStrike.PNG') no-repeat;"></option>			
+			<option style="background:url('../images/Camel.PNG') no-repeat;"</option>			
 		</select>
+		
 		
 		
 		<br><br>
