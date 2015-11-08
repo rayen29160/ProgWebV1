2933d416aeaf22a1c82ec993454e5ce154d6ffc7
toscer-f <toscer-f@saint-louis29.net>
2015-11-08 22:08:50 +0100

    css again

--- vues/v_accueil.php
+++ vues/v_accueil.php
@@ -13,13 +13,13 @@
 		<div id = texte>
 			<?php 
 				echo("<strong><h1>Bienvenue ".$_SESSION["id"]."</h1></strong>");
-				echo("<SPAN>Depuis que vous fumez vous avez dépensé environ <br> <h1>".number_format($_SESSION["argentDepense"], 0, ',', ' ')."€</h1> </SPAN>");		
+				echo("<SPAN>Depuis que vous fumez vous avez dépensé environ <br></SPAN> <h1>".number_format($_SESSION["argentDepense"], 0, ',', ' ')."€</h1><br>");		
 				echo "<BR>";
 				if($_SESSION["reveDepasse"]>=1) {
-					echo("<SPAN>Avec cet argent vous auriez pu payer ".$_SESSION["reveDepasse"]." fois votre ".$_SESSION["objLong"]."</SPAN>");
+					echo("<SPAN id=ici>Avec cet argent vous auriez pu payer ".$_SESSION["reveDepasse"]." fois votre ".$_SESSION["objLong"]."</SPAN>");
 				} else {
 					if($_SESSION["reveDepasse"]<1 && $_SESSION["reveDepasse"]>=0.75) {
-						echo("<SPAN>Avec cet argent vous auriez presque pu payer votre ".$_SESSION["objLong"]."</SPAN>");
+						echo("<SPAN id=ici>Avec cet argent vous auriez presque pu payer votre ".$_SESSION["objLong"]."</SPAN>");
 					}
 				}		
 			?>
